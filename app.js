1fd23da997b0fa040b6d33b0955a9f380fa386e9
Anton Jonsson <94antjon@skola.boras.se>
2023-01-16 11:29:56 +0100

    add new object containga a string, number and bool

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 "use strict";
 const animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
+const obj = { name: "gugge", age: 20, ishappy: true };
 console.log(animals);
+console.log(obj);
--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
 const animals: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
+const obj: {} = { name: "gugge", age: 20, ishappy: true };
+
 console.log(animals);
+console.log(obj);
