31a2d57c3b51f90f31b634fac2d3452c19a9f3ae
Anton Jonsson <94antjon@skola.boras.se>
2023-01-16 11:50:21 +0100

    added function with 1 parameter

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 "use strict";
 const animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
 const obj = { name: "gugge", age: 20, ishappy: true };
+function Hello(name) {
+    if (name === "X") {
+        return "hej X";
+    }
+    else {
+        console.log("whazaaaah");
+    }
+}
+Hello("X");
+Hello("Sup");
 console.log(animals);
 console.log(obj);
--- app.ts
+++ app.ts
@@ -2,5 +2,16 @@ const animals: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
 const obj: {} = { name: "gugge", age: 20, ishappy: true };
 
+function Hello(name: string) {
+    if (name === "X") {
+        return "hej X";
+    } else {
+        console.log("whazaaaah");
+    }
+}
+
+Hello("X");
+Hello("Sup");
+
 console.log(animals);
 console.log(obj);
